71f5dcec35c632b22246fa3ca569af4a00ecfd7d
Reapard <Reapard@users.noreply.github.com>
2016-02-17 12:54:11 +0200

    First commit

---
+++ simple-todos.js
@@ -0,0 +1,19 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.body.helpers({
+    tasks:[
+      {text : "Task 1"},
+      {text : "Task 2"},
+      {text : "Task 3"}
+    ]
+  });
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
